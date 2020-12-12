a7c443ee8539b2bed15217154d210743b6eda046
Philip Karlsson <philip.karlsson@husqvarnagroup.com>
2020-12-12 13:20:38 +0100

    Now have a way of receiving input

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import argparse
+import curses
+import os
 
 class Trainer():
-    def __init__(self):
-        pass
+    def __init__(self, win):
+        self.win = win
 
-def main():
+    def train(self):
+        self.win.nodelay(True)
+        key=""
+        self.win.clear()
+        self.win.addstr("Detected key:")
+        while 1:
+            try:
+               key = self.win.getkey()
+               self.win.clear()
+               self.win.addstr("Detected key:")
+               self.win.addstr(str(key))
+               if key == os.linesep:
+                  break
+            except Exception as e:
+               # No input
+               pass
+
+def main(win):
     parser = argparse.ArgumentParser(description='Tool to practice touch typing on my new ergo..')
     parser.add_argument('-b', '--buildcmd', help='Command used to build the project', required=False, nargs='+')
+    args = parser.parse_args()
+    print(args)
 
-if __name__ == "__main__":
-    main()
+    t = Trainer(win)
+    t.train()
+
+curses.wrapper(main)
